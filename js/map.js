b42170e7f793d600ee6e95afe5f7abd123b8d396
sarahperez1 <snperez@g.ucla.edu>
2021-08-05 10:37:00 -0700

    changes

--- js/map.js
+++ js/map.js
@@ -1,9 +1,23 @@
-var map = L.map('map').setView([33.9401,-118.1332], 17);
+// Global variables
+let map;
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+// path to csv data
+let markers = L.featureGroup();
 
-var marker = L.marker([33.9401,-118.1332]).addTo(map)
-		.bindPopup('Downey, CA <br> Where Sarah is residing')
-		.openPopup();	
+
+// initialize
+$( document ).ready(function() {
+    createMap();
+});
+
+// create the map
+function createMap(){
+	map = L.map('map').setView([34.05,-118.115],10);
+
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+}
+
+
+
